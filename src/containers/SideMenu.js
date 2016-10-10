0e9dec729ae6b4235da05b4abc4a45c8e196ada8
Ian Fisk <i.fisk32@gmail.com>
2016-10-09 23:21:27 -0700

    Change to class syntax and use Animated events

--- src/containers/SideMenu.js
+++ src/containers/SideMenu.js
@@ -3,6 +3,7 @@
 
 import React, { Component } from 'react';
 import {
+  Animated,
   PanResponder,
   StyleSheet,
   Text,
@@ -12,98 +13,53 @@ import Home from '../components/Home';
 import Main from '../components/Main';
 
 class SideMenuClass extends Component {
-  componentWillMount() {
-    this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: this._handleStartShouldSetPanResponder.bind(this),
-      onMoveShouldSetPanResponder: this._handleMoveShouldSetPanResponder.bind(this),
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pan: new Animated.ValueXY()
+    };
+
+    this.state.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
       onPanResponderGrant: this._handlePanResponderGrant.bind(this),
-      onPanResponderMove: this._handlePanResponderMove.bind(this),
+      onPanResponderMove: Animated.event([null, {
+        dx: this.state.pan.x, // x,y are Animated.Value
+        dy: this.state.pan.y, // this.state.pan.y = gestureState.dy (binds)
+      }]),
       onPanResponderRelease: this._handlePanResponderEnd.bind(this),
       onPanResponderTerminate: this._handlePanResponderEnd.bind(this),
     });
-    this._previousLeft = 150;
-    this._previousTop = 200;
-    this._squareStyles = {
-      style: {
-        left: this._previousLeft,
-        top: this._previousTop,
-      }
-    };
-
-    console.log("*****************************HELLLLLLOOOO")
-    this._highlight();
-    console.log("*****************************GOOOOODBYEEEE")
   }
-
-  componentDidMount() {
-  }
-
+  
   render() {
     return (
-      <View style={this.props.style}>
-        <View
-          ref={(square) => {
-            this.square = square;
-          }}
-          style={styles.square}
-          {...this._panResponder.panHandlers}>
-        </View>
-      </View>
+      <Animated.View
+        {...this.state.panResponder.panHandlers}
+        style={[this.state.pan.getLayout(), styles.menu]}>
+        {this.props.children}
+      </Animated.View>
     )
   }
 
-  _highlight() {
-    console.log("*****************************HIGHLIGHTINGGGGGG")
-    this._squareStyles.style.backgroundColor = 'blue';
-    this._updateNativeStyles();
-  }
-
-  _unHighlight() {
-    console.log("*****************************UNNNHIGHLIGHTINGGGGGG")
-    this._squareStyles.style.backgroundColor = 'green';
-    this._updateNativeStyles();
-  }
-
-  _updateNativeStyles() {
-    this.square && this.square.setNativeProps(this._squareStyles);
-  }
-
-  _handleStartShouldSetPanResponder(e: Object, gestureState: Object): boolean {
-    // Should we become active when the user presses down on the square?
-    return true;
-  }
-
-  _handleMoveShouldSetPanResponder(e: Object, gestureState: Object): boolean {
-    // Should we become active when the user moves a touch over the square?
-    return true;
-  }
-
   _handlePanResponderGrant(e: Object, gestureState: Object) {
-    console.log("*****************************PanResponder GRANT")
-    this._highlight();
+    // offset == the origin, value == the amount added to the origin
+    console.log("*****************************PanResponder GRANT");
+    this.state.pan.setOffset({x: this.state.pan.x._value, y: this.state.pan.y._value});
   }
-  _handlePanResponderMove(e: Object, gestureState: Object) {
-    console.log("*****************************PanResponder MOVE")
-    this._squareStyles.style.left = this._previousLeft + gestureState.dx;
-    this._squareStyles.style.top = this._previousTop + gestureState.dy;
-    this._updateNativeStyles();
-  }
+
   _handlePanResponderEnd(e: Object, gestureState: Object) {
-    console.log("*****************************PanResponder END")
-    this._unHighlight();
-    this._previousLeft += gestureState.dx;
-    this._previousTop += gestureState.dy;
+    // pan.{x,y}._value == gestureState.{dx,dy} so we need to add the distance of the pan to the offset and set the new values
+    console.log("*****************************PanResponder END");
+    this.state.pan.flattenOffset();
   }
 }
 
 var styles = StyleSheet.create({
-  square: {
-    width: 100,
-    height: 100,
-    position: 'absolute',
-    backgroundColor: 'green',
-    left: 150,
-    top: 200,
+  menu: {
+    flex: 1,
+    backgroundColor: 'powderblue',
   },
 });
 
